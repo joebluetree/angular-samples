08ac9b59fe8d174711daaa90d2e18f4729c76b85
joe <joe.bluetree@gmail.com>
2023-03-10 16:53:59 +0530

    save

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-export const routes: Routes = [
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'contactus', component: ContactusComponent },
 ]
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    ContactusComponent,
+    AboutusComponent
   ],
   imports: [
     CommonModule,
