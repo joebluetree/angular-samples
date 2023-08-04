097fe91bb1d615bea91de42a4d26b206859b74e8
joe <joe.bluetree@gmail.com>
2023-08-04 16:57:41 +0530

    save

--- src/app/shared/controls/autocomplete/auto-complete-modulem/auto-complete-modulem.component.ts
+++ src/app/shared/controls/autocomplete/auto-complete-modulem/auto-complete-modulem.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, forwardRef } from '@angular/core';
+import { Component, ElementRef, Input, QueryList, ViewChild, ViewChildren, forwardRef } from '@angular/core';
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { iModulem } from '../../../../user-admin/models/imodulem';
 import { CommonService } from 'src/app/shared/services/common.service';
 import { GlobalService } from 'src/app/core/services/global.service';
 
+import { Subject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'auto-complete-modulem',
@@ -19,11 +21,12 @@ import { GlobalService } from 'src/app/core/services/global.service';
 })
 export class AutoCompleteModulemComponent implements ControlValueAccessor {
   @Input('id') id: string = '_id_text';
-  @Input() case: string = 'upper';
+  @Input() case: string = '';
 
+  @ViewChild('inputBox') inputBox: ElementRef;
+  @ViewChildren('radio') inputs: QueryList<ElementRef>;
 
-  showDiv = true;
-
+  showDiv = false;
   isDisabled = false;
   ctrl = new FormControl('');
 
@@ -31,52 +34,133 @@ export class AutoCompleteModulemComponent implements ControlValueAccessor {
   onTouch: any = () => { };
 
   records: Array<iModulem> = [];
+  term$ = new Subject<string>();
+
+  isChanged = false;
+
+  rowid = 0;
 
   constructor(private service: CommonService, private gs: GlobalService) {
   }
 
   ngOnInit(): void {
-    this.loadData();
+    this.setup();
   }
 
-  loadData() {
-    let search_record = {
-      'table': 'modulem',
-      'company_id': this.gs.user.user_company_id
-    }
-    this.service.getList(search_record).subscribe({
-      next: (v) => {
-        console.log(v.records);
-        this.records = <iModulem[]>v.records;
-      },
-      error: (e) => {
-        this.gs.showScreen([e.error || e.message]);
-      },
-      complete: () => { }
-    })
+  private setup() {
+    this.term$
+      .pipe(
+        switchMap(value => {
+          let search_record = {
+            'table': 'modulem',
+            'company_id': this.gs.user.user_company_id,
+            'module_name': value
+          }
+          return this.service.getList(search_record);
+        }),
+      ).subscribe({
+        next: (v) => {
+          this.records = <iModulem[]>v.records;
+          if (this.records.length > 0) {
+            this.showDiv = true;
+            this.selectTableRow(0);
+          }
+          this.isChanged = false;
+        },
+        error: (e) => {
+          this.gs.showScreen([e.error || e.message]);
+        },
+        complete: () => { }
+      })
+  }
+
+  private searchRecord() {
+    let data = this.ctrl.value?.toString().trim() || '';
+    this.term$.next(data);
+  }
+
+  selectTableRow(index: number = 0) {
+    console.log(this.inputs);
+    setTimeout(() => {
+      if (this.inputs.toArray()[index])
+        this.inputs.toArray()[index].nativeElement.focus();
+    }, 0);
+  }
+
+  selectInputBox() {
+    setTimeout(() => {
+      this.inputBox.nativeElement.focus();
+    }, 0);
+
+  }
+
+  ngOnDestroy(): void {
+    this.term$.unsubscribe();
   }
 
   writeValue(obj: string): void {
     this.ctrl.setValue(obj);
   }
+
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any): void {
     this.onTouch = fn;
   }
 
-  onBlur: any = () => {
-    let _data = this.ctrl.value?.toString().trim() || '';
-    if (this.case.toString().toLowerCase() == 'upper') {
-      _data = _data.toUpperCase();
-    }
-    if (this.case.toString().toLowerCase() == 'lower') {
-      _data = _data.toLowerCase();
-    }
-    this.writeValue(_data);
-    this.onChange(_data);
-    this.onTouch();
+  public InputBox_OnFocus: any = () => {
+    this.isChanged = false;
+    this.showDiv = false;
   }
+
+  public InputBox_OnKeyDown(event: KeyboardEvent) {
+    console.log('onkey down changed', event);
+    if (event.key == "Tab" || event.key == "Escape")
+      return;
+    if (event.key == "ArrowDown" || event.key == "Enter") {
+      this.searchRecord();
+    }
+    else
+      this.isChanged = true;
+  }
+
+  public InputBox_OnBlur: any = () => {
+    if (this.isChanged) {
+      this.searchRecord();
+    }
+  }
+
+  radioKeyDown(event: KeyboardEvent, rec: iModulem) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+    }
+    if (event.key === 'Enter') {
+      this.onSelection(rec);
+    }
+    if (event.key === 'Escape') {
+      this.cancelSelection();
+    }
+    if (event.key === 'PageUp') {
+    }
+    if (event.key === 'PageDown') {
+    }
+  }
+
+  onSelection(rec: iModulem) {
+    this.ctrl.setValue(rec.module_name);
+    this.isChanged = false;
+    this.showDiv = false;
+    this.selectInputBox();
+  }
+
+  cancelSelection() {
+    this.isChanged = false;
+    this.showDiv = false;
+    this.selectInputBox();
+  }
+
+
 
 }
